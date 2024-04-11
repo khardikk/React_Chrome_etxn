f885b13890f6a0850bf50bac00b76bb5dbf0b119
hardikk kamboj <kambojpushkal@gmail.com>
2024-04-11 22:16:13 +0530

    Update App.tsx with new title and additional code comments

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React Basic Color Picker Extension</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -24,6 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>This is to practice <code>Chrome Web APIS</code></p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
